c79c6c40f13f587551556fde511918dc4871617e
ANDERSON CARLOS BRÜSKE JÚNIOR <anderson.junior@univille.br>
2025-03-18 21:51:40 -0300

    feat: revisao mer

---
+++ revisao/src/main/java/br/univille/entity/Pessoa.java
@@ -0,0 +1,72 @@
+package br.univille.entity;
+public class Pessoa {
+    //variavel, atributo
+    private long id;
+    private String nome; 
+    private String endereco;
+    
+    private Cidade cidade;
+    
+    
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+
+    //Construtor (mesmo nome da classe, não tem)
+    public Pessoa(String nome){
+        //this referencia a classe
+         this.nome = nome;
+    }
+
+
+    //Dois metodos com assinatura semelhante = polimorfismo
+    public Pessoa(){
+
+    }
+
+
+    // metodo
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    //Sobreescrita de metodo(overwrite)
+    @Override
+    public String toString(){
+
+        return getNome();
+    }
+
+}
